94c4d390d278da581a72e0619c318555b698b8e3
Kelly Zhou <kelly.zhou@student.unsw.edu.au>
2022-09-25 11:07:36 +1000

    updating menu items and categories to include display bool and image

--- website/category.py
+++ website/category.py
@@ -1,4 +1,5 @@
 class Category:
-    def __init__(self, name, display_order = 0):
+    def __init__(self, name, display = False, display_order = 0):
         self.name = name
+        self.display = display
         self.display_order = display_order
--- website/menu_item.py
+++ website/menu_item.py
@@ -1,9 +1,11 @@
 class MenuItem:
-    def __init__(self, name, cost, category, display_order = 0, desc = "", ingredients = "", tags = []):
+    def __init__(self, name, cost, category, display = False, display_order = 0, desc = "", ingredients = "", tags = [], img = None):
         self.name = name
         self.cost = cost
         self.category = category
+        self.display = display
         self.display_order = display_order
         self.desc = desc
         self.ingredients = ingredients
         self.tags = tags
+        self.img = img
