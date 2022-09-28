7941ac09d273a21c67933d94fa852602bbb1f477
Joshua Kasing Lee <joshualeee2018@gmail.com>
2022-09-28 11:03:12 +1000

    comment main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from website import create_app
 
 app = create_app()
 
+#initialise restaurant class here (constructing it. it should be global)
+
 if __name__ == '__main__':
     app.run(debug=True)
