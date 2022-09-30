02c726102485fb1ffba177786cf2c101eede1ef9
Joshua Kasing Lee <joshualeee2018@gmail.com>
2022-09-30 10:38:26 +1000

    inserts

---
+++ dbinserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO staff(id, role, password) values (1, 'wait', 'waiterA0five'), (2, 'kitchen', 'kitchenA0five'), (3, 'manager', 'managerA0five');
+
+INSERT INTO leaderboard_entry(id, email, score, time_played) values (1, 'monkey@gmail.com', 20, date(20220108::text)), (2, 'koala@gmail.com', 50, date(20220108::text)), (3, 'tiger@gmail.com', 100, date(20220108::text));
